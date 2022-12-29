ee5589e6149d750abfca813478b620080c3b3934
biruk_anley <birukanley2022@gmail.com>
2022-12-29 16:56:56 +0300

    add hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,5 +1,6 @@
 import Katie from '../images/Katie.png'
 import star from '../images/star.svg'
+import '../App.css'
 
 
 
@@ -7,8 +8,8 @@ const Hero =(props)=>{
     console.log('props',props)
     return (
         <div className="Hero">
-            <img src={Katie} />
-            <img src={`../images/${props.img}`} />
+           
+            <img src={`../images/${props.imgs}`} />
             
             <div className='card--status'>
                 <span> <img src={star}/>  </span>
