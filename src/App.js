f916855c95d2334f1659e9c4048ddcbb9d14bc82
biruk_anley <birukanley2022@gmail.com>
2022-12-30 22:31:46 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -14,14 +14,19 @@ const App = () => {
     return (
       <Hero
         //key is for manipulating difference..wanring will be removed
-          key={item.id}
-          imgs={item.coverImg}
-          rating={item.stats.rating}
-          reviewCount={item.reviewCount}
-          location={item.location}
-          title={item.title}
-          price={item.price}
-          openSpots = {item.openSpots}
+        // here is the first way to pass props
+          // key={item.id}
+          // imgs={item.coverImg}
+          // rating={item.stats.rating}
+          // reviewCount={item.reviewCount}
+          // location={item.location}
+          // title={item.title}
+          // price={item.price}    this can be expressed usign spread operator  ...{item}
+          // openSpots = {item.openSpots}
+        
+        // here is another way of passing props
+
+        item = {item}
         />
     );
   });
