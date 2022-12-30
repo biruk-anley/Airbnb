696a9a4bdfadf6e10da27e55c53ffa184f08f736
biruk_anley <birukanley2022@gmail.com>
2022-12-30 18:11:56 +0300

    add data.js file which act as database

--- src/data.js
+++ src/data.js
@@ -30,7 +30,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "logo.png",
+        coverImg: "Katie.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
