798300e0dd762904c53a88bb01626007446b8c5a
biruk_anley <birukanley2022@gmail.com>
2023-01-06 11:02:54 +0300

    update

--- src/App.js
+++ src/App.js
@@ -13,19 +13,6 @@ const App = () => {
   const cards = data.map(item => {
     return (
       <Hero
-        //key is for manipulating difference..wanring will be removed
-        // here is the first way to pass props
-          // key={item.id}
-          // imgs={item.coverImg}
-          // rating={item.stats.rating}
-          // reviewCount={item.reviewCount}
-          // location={item.location}
-          // title={item.title}
-          // price={item.price}    this can be expressed usign spread operator  ...{item}
-          // openSpots = {item.openSpots}
-        
-        // here is another way of passing props
-
         item = {item}
         />
     );
